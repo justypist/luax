f07f7297819d68d8960107b2876406a7937c4f04
上帝的猫 <1003659191@qq.com>
2019-06-26 01:42:03 +0800

    函数式接口 fold

---
+++ modules/standard/functional/fold.lua
@@ -0,0 +1,11 @@
+--[[
+    规约
+    fold(table,any,(any,any,string|number,table)=>any):any
+]]
+local function fold(tbl,initValue,callback)
+    for key,value in pairs(tbl) do
+        initValue=callback(initValue,value,key,tbl);
+    end
+    return initValue;
+end
+return fold;
