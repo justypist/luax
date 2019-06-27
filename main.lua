854c3197c4bc180f761460e4518bfe0b8546c185
上帝的猫 <1003659191@qq.com>
2019-06-27 22:59:00 +0800

    去除main.lua的修改监听 暂时作为调试测试用

--- main.lua
+++
@@ -1,7 +0,0 @@
--- 首先导入import modules 等模块
-_G.import=require("modules/standard/core/import")
-_G.modules=import("modules/standard/core/modules")
--- Application
-local clone=import("modules/standard/base/clone")
-local t={1,2,3}
-print(clone(t)==t and "true" or "false")
