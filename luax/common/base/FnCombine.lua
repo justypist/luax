6b3f113fd0b02ba74d0382d79f25ed6382714e9e
上帝的猫 <1003659191@qq.com>
2019-07-16 21:02:18 +0800

    fix bug: 使用正确的导入functional的方法

--- luax/common/base/FnCombine.lua
+++ luax/common/base/FnCombine.lua
@@ -3,18 +3,17 @@
     函数合并
     FnCombine(...<function>):function
 ]]
--- local functional=import("../functional");
--- print("functional",functional)
--- local foreach,reduce=functional.foreach,functional.reduce;
--- local function FnCombine(...)
---     local fns={...};
---     foreach(fns,function(fn)
---         assert(type(fn)=="function","FnCombine 参数只能包含可变数量function")
---     end);
---     return reduce(fns,function(left,right)
---         return function(...)
---             return right(left(...));
---         end;
---     end);
--- end
+local functional=import("../functional/init");
+local foreach,reduce=functional.foreach,functional.reduce;
+local function FnCombine(...)
+    local fns={...};
+    foreach(fns,function(fn)
+        assert(type(fn)=="function","FnCombine 参数只能包含可变数量function")
+    end);
+    return reduce(fns,function(left,right)
+        return function(...)
+            return right(left(...));
+        end;
+    end);
+end
 return FnCombine;
