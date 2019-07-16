f98c1a0ff1b9616d29e16eae16a1cd4d14fce057
上帝的猫 <1003659191@qq.com>
2019-07-16 22:37:13 +0800

    update module: Class

--- luax/oop/core/Class.lua
+++ luax/oop/core/Class.lua
@@ -1,4 +1,13 @@
 --[[
-    Class
+    Class(name):
     类
 ]]
+local Context=import("Context");
+local function Class(ClassName)
+    return function(ClassBody)
+        local ctx=Context();
+        ClassBody(ctx);
+        return ctx;
+    end
+end
+return Class;
