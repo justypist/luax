65c977d82fe94e1766633e036fbee5cb9289e588
simplessor <1003659191@qq.com>
2019-12-03 11:39:26 +0800

    禁止创建全局变量 禁止修改元表

--- LUAX.lua
+++ LUAX.lua
@@ -21,4 +21,25 @@ local functional=import("luax/common/functional/init");
 -- 字符化
 ---
 local stringify=import("luax/common/base/stringify");
+local dump = import("luax/common/base/dump")
 ---
+
+-- 元表操作
+--
+local meta = import("luax/oop/core/Meta")
+--
+
+-- 添加监听
+-- 禁止直接注册全局环境
+meta.setMetaField(_ENV,"__newindex",function(ENV,k,v)
+    error("禁止创建全局变量",2)
+end)
+-- 禁止访问不存在的全局环境
+meta.setMetaField(_ENV,"__index",function(ENV,k)
+    local v = rawget(ENV,k)
+    assert(v,"全局变量["..stringify(k).."]不存在",2)
+    return v
+end)
+-- 禁止修改全局环境的元表
+meta.setMetaField(_ENV,"__metatable",true)
+--
