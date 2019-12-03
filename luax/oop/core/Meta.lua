fd14391b5037c8c792c6b5617c3e89060a49f3cb
simplessor <1003659191@qq.com>
2019-12-03 11:39:08 +0800

    放弃使用映射表 直接使用原始字段名

--- luax/oop/core/Meta.lua
+++ luax/oop/core/Meta.lua
@@ -5,22 +5,18 @@
 local getmetatable,setmetatable=getmetatable,setmetatable;
 local rawget,rawset=rawget,rawset;
 -- 元表中字段映射
-local MetaField=import("MetaField");
+-- local MetaField=import("MetaField");
 -- 获取元表中某一字段的值
 -- getMetaField(table,string):table|function|nil
 local function getMetaField(t,fieldName)
-    local realMetaFieldName=MetaField[fieldName];
-    assert(realMetaFieldName,"getMetaField 元字段不存在于MetaField");
     local meta=getmetatable(t) or {};
-    return rawget(meta,realMetaFieldName);
+    return rawget(meta,fieldName);
 end
 -- 设定元表中某一字段的值
 -- setMetaField(table,string,function|table|nil):table
 local function setMetaField(t,fieldName,fieldValue)
-    local realMetaFieldName=MetaField[fieldName];
-    assert(realMetaFieldName,"getMetaField 元字段不存在于MetaField");
     local meta=getmetatable(t) or {};
-    rawset(meta,realMetaFieldName,fieldValue);
+    rawset(meta,fieldName,fieldValue);
     return setmetatable(t,meta);
 end
 local Meta={
