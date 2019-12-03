cdae5257d1d6f36e4121fd6f7e20a8128c8a0c36
simplessor <1003659191@qq.com>
2019-12-03 23:07:46 +0800

    Number tostring

--- luax/class/base/Number.lua
+++ luax/class/base/Number.lua
@@ -8,8 +8,13 @@ Number:attr("value",0)
 Number:constructor(function(this,num)
     this.value = tonumber(num)
 end)
+-- todo
+-- 进制转换
 
 Number:method("toString",function(this)
     return tostring(this.value)
+end)
+Number:meta("__tostring",function(this)
+    return this:toString()
 end)
 return Number:build()
