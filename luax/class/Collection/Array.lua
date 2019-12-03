24e1a23f3793c5a164b4cea41b59dd8b17d6c807
simplessor <1003659191@qq.com>
2019-12-03 22:55:07 +0800

    Array 数组

---
+++ luax/class/Collection/Array.lua
@@ -0,0 +1,13 @@
+--[[
+    Array
+    数组
+]]
+local Object = import("Object")
+local Array = Object:drive("Array")
+
+Array:attr("value",{})
+Array:constructor(function(this)
+
+end)
+
+return Array:build()
