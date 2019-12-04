ca2d1c1135cedb17c6941f2215c76d492641d0cc
simplessor <1003659191@qq.com>
2019-12-04 13:41:44 +0800

    容器类

--- luax/class/collection/Collection.lua
+++ luax/class/collection/Collection.lua
@@ -9,7 +9,40 @@ Collection:attr("value",{})
 Collection:constructor(function(this,value)
     this.value = value or this.value
 end)
-Collection:method("foreach",function(this,fn)
 
+-- 实例方法
+Collection:method("foreach",function(this,fn)
+    functional.foreach(this.value,fn)
+    return this
 end)
+Collection:method("map",function(this,fn)
+    functional.map(this.value,fn)
+    return this
+end)
+Collection:method("filter",function(this,fn)
+    functional.filter(this.value,fn)
+    return this
+end)
+Collection:method("find",function(this,fn)
+    return functional.find(this.value,fn)
+end)
+Collection:method("reduce",function(this,fn)
+    return functional.reduce(this.value,fn)
+end)
+Collection:method("every",function(this,fn)
+    return functional.every(this.value,fn)
+end)
+Collection:method("some",function(this,fn)
+    return functional.some(this.value,fn)
+end)
+
+-- 静态方法
+Collection:static("foreach",functional.foreach)
+Collection:static("map",functional.map)
+Collection:static("filter",functional.filter)
+Collection:static("find",functional.find)
+Collection:static("reduce",functional.reduce)
+Collection:static("every",functional.every)
+Collection:static("some",functional.some)
+
 return Collection:build()
