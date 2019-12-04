42091f5312b258a44e9c7b66641d6042c8606ff9
simplessor <1003659191@qq.com>
2019-12-04 09:23:36 +0800

    基础类型添加get/set

--- luax/class/base/Base.lua
+++ luax/class/base/Base.lua
@@ -8,5 +8,12 @@ Base:attr("value",nil)
 Base:constructor(function(this,value)
     this.value = value
 end)
+Base:method("setValue",function(this,value)
+    this.value = value
+    return this
+end)
+Base:method("getValue",function(this)
+    return this.value
+end)
 
 return Base:build()
