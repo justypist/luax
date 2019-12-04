8ca38aa61fe97d2980debefe11469d4fdcd4bd30
simplessor <1003659191@qq.com>
2019-12-04 09:33:12 +0800

    构造函数运行过程 添加注释

--- luax/oop/core/builderMethod.lua
+++ luax/oop/core/builderMethod.lua
@@ -30,17 +30,28 @@ local builderMethod = {
             return builder
         end,
         build=function(builder)
+            -- 将静态属性作为class主体
             local class = builder[KW.STATIC]
+            -- 获取父类
             local parentClass = builder[KW.PARENT]
+            -- 为class主体添加元表（赋予Class特性）
             setmetatable(class,builder[KW.METAFIELD])
+            -- 提取构造函数
             local constructor = builder[KW.METAFIELD]["__call"] or Noop
+            -- 装饰构造函数
             builder[KW.METAFIELD]["__call"] = function(class,...)
+                -- 获取父类中的属性
                 local parentAttr = (getmetatable(parentClass) or {})[KW.ATTRIBUTE] or {}
+                -- 获取当前类的属性
                 local thisatr = clone(builder[KW.METAFIELD][KW.ATTRIBUTE])
+                -- 将父类属性与当前类属性整合为完整的类属性
                 local this = clone(parentAttr)
                 foreach(thisatr,function(v,k)this[k]=v;end)
+                -- 调用构造函数 this为对象完整属性，此时其不具备对象方法
                 constructor(this,...)
+                -- 赋予对象方法
                 setmetatable(this,builder[KW.METAFIELD])
+                -- 返回完整的对象实例
                 return this
             end
             return class
