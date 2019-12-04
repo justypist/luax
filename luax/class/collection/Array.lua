c2aac3b7c1e835bea5304a4148188764fef8edba
simplessor <1003659191@qq.com>
2019-12-04 13:56:40 +0800

    Array: insert/remove/length/empty

--- luax/class/collection/Array.lua
+++ luax/class/collection/Array.lua
@@ -5,8 +5,27 @@
 local Array = import("Collection"):drive("Array")
 
 Array:attr("value",{})
-Array:constructor(function(this)
-
+Array:constructor(function(this,value)
+    this.value = value or this.value
+end)
+Array:method("insert",function(this,value)
+    table.insert(this.value,value)
+    return this
+end)
+Array:method("remove",function(this,index)
+    index = index or #this.value
+    if index>#this.value or index<1 then
+        error("数组越界,数组长度为"..#this.value.."，而即将去除的索引位置为"..index,2)
+    end
+    table.remove(this.value,index)
+    return this
+end)
+Array:method("length",function(this)
+    return #this.value
+end)
+Array:method("empty",function(this)
+    this.value={}
+    return this
 end)
 
 return Array:build()
