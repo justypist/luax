806d1b9e0752bc0599239404764f51c7689182dc
simplessor <1003659191@qq.com>
2019-12-04 16:03:47 +0800

    Array 支持栈 队列

--- luax/class/collection/Array.lua
+++ luax/class/collection/Array.lua
@@ -8,6 +8,9 @@ Array:attr("value",{})
 Array:constructor(function(this,value)
     this.value = value or this.value
 end)
+Array:method("get",function(this,index)
+    return this.value[index]
+end)
 Array:method("insert",function(this,index,value)
     if value==nil then
         table.insert(this.value,index)
@@ -18,7 +21,7 @@ Array:method("insert",function(this,index,value)
 end)
 Array:method("remove",function(this,index)
     index = index or #this.value
-    if index>#this.value or index<1 then
+    if index>this:length() or index<1 then
         error("数组越界,数组长度为"..#this.value.."，而即将去除的索引位置为"..index,2)
     end
     return table.remove(this.value,index)
@@ -26,15 +29,33 @@ end)
 Array:method("length",function(this)
     return #this.value
 end)
-Array:method("empty",function(this)
+Array:method("clear",function(this)
     this.value={}
     return this
 end)
+-- 栈操作
 Array:method("push",function(this,value)
     return this:insert(value)
 end)
 Array:method("pop",function(this)
     return this:remove()
+end)
+-- 队列操作
+Array:method("enqueue",function(this,value)
+    this.push(value)
+    return this
+end)
+Array:method("dequeue",function(this)
+    return this:remove(1)
+end)
+Array:method("isEmpty",function(this)
+    return this:length()==0
+end)
+Array:method("first",function(this)
+    return this:get(1)
+end)
+Array:method("last",function(this)
+    return this:get(this:length())
 end)
 
 return Array:build()
